5846ef1ca13845a49e029eb718eb77619f0e4bfe
Beast1989 <jinyang1989@outlook.com>
2022-10-07 14:31:35 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+//导入全局样式表
+import './assets/css/global.css'
+
+import axios from 'axios'
+Vue.prototype.$http = axios
+axios.defaults.baseURL='http://47.108.251.106:8881/api/private/v1/login'
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
